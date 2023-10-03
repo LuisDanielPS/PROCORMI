2f14fb1d2e14e3093ecb1d4cbb9dbfa8b393ff03
LuisDanielPS <105894205+LuisDanielPS@users.noreply.github.com>
2023-10-02 21:02:02 -0600

    BD actualizada

--- Script BD/Script_PROCORMI.sql
+++ Script BD/Script_PROCORMI.sql
@@ -18,6 +18,29 @@ USE `procormi`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `actions`
+--
+
+DROP TABLE IF EXISTS `actions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `actions` (
+  `Id_Actions` int NOT NULL AUTO_INCREMENT,
+  `Action_Description` varchar(500) NOT NULL,
+  PRIMARY KEY (`Id_Actions`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actions`
+--
+
+LOCK TABLES `actions` WRITE;
+/*!40000 ALTER TABLE `actions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `actions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `answer`
 --
 
@@ -25,9 +48,10 @@ DROP TABLE IF EXISTS `answer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `answer` (
-  `Id_Answer` int NOT NULL,
+  `Id_Answer` int NOT NULL AUTO_INCREMENT,
   `Text` varchar(400) NOT NULL,
   `Id_Question` int NOT NULL,
+  `Answer_Counter` int DEFAULT NULL,
   PRIMARY KEY (`Id_Answer`),
   KEY `Fk_Question_Answer_idx` (`Id_Question`),
   CONSTRAINT `Fk_Question_Answer` FOREIGN KEY (`Id_Question`) REFERENCES `question` (`Id_Question`)
@@ -41,6 +65,29 @@ CREATE TABLE `answer` (
 LOCK TABLES `answer` WRITE;
 /*!40000 ALTER TABLE `answer` DISABLE KEYS */;
 /*!40000 ALTER TABLE `answer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `errors`
+--
+
+DROP TABLE IF EXISTS `errors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `errors` (
+  `Id_Error` int NOT NULL AUTO_INCREMENT,
+  `Error_Description` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`Id_Error`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `errors`
+--
+
+LOCK TABLES `errors` WRITE;
+/*!40000 ALTER TABLE `errors` DISABLE KEYS */;
+/*!40000 ALTER TABLE `errors` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -58,7 +105,7 @@ CREATE TABLE `files` (
   `File_Size` int DEFAULT NULL,
   `Creation_Date` datetime NOT NULL,
   PRIMARY KEY (`File_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,6 +114,7 @@ CREATE TABLE `files` (
 
 LOCK TABLES `files` WRITE;
 /*!40000 ALTER TABLE `files` DISABLE KEYS */;
+INSERT INTO `files` VALUES (8,'00_SC_702_DISPOSICIONES GENERALES.pdf',NULL,'application/pdf',5,'2023-10-01 15:45:23'),(9,'00_SC_702_DISPOSICIONES GENERALES.pdf',NULL,'application/pdf',5,'2023-10-01 15:50:41');
 /*!40000 ALTER TABLE `files` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -78,7 +126,7 @@ DROP TABLE IF EXISTS `poll`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `poll` (
-  `Id_Poll` int NOT NULL,
+  `Id_Poll` int NOT NULL AUTO_INCREMENT,
   `Name` varchar(45) DEFAULT NULL,
   `Description` varchar(45) DEFAULT NULL,
   `Creation_Date` datetime DEFAULT NULL,
@@ -129,12 +177,12 @@ CREATE TABLE `project` (
   `Id_Project` int NOT NULL AUTO_INCREMENT,
   `Project_Name` varchar(100) NOT NULL,
   `Description_Project` varchar(1000) NOT NULL,
-  `Id_State` int NOT NULL,
+  `Id_Status` int NOT NULL,
   `Date_Creation` datetime NOT NULL,
   PRIMARY KEY (`Id_Project`),
-  KEY `Fk_State_idx` (`Id_State`),
-  CONSTRAINT `Fk_State_Project` FOREIGN KEY (`Id_State`) REFERENCES `state` (`Id_State`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `Fk_Status_idx` (`Id_Status`),
+  CONSTRAINT `Fk_Status_Project` FOREIGN KEY (`Id_Status`) REFERENCES `status` (`Id_Status`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -143,7 +191,7 @@ CREATE TABLE `project` (
 
 LOCK TABLES `project` WRITE;
 /*!40000 ALTER TABLE `project` DISABLE KEYS */;
-INSERT INTO `project` VALUES (15,'SSSS','AAAA\n',1,'2023-09-30 14:15:16'),(16,'222','2222222\n',1,'2023-09-30 14:19:46'),(17,'1982892819282','2121222\n',1,'2023-09-30 14:21:54'),(19,'sass','sassss\n',1,'2023-09-30 14:31:47'),(20,'ssssss','12222222222222222222\n',1,'2023-09-30 14:34:39');
+INSERT INTO `project` VALUES (15,'123','AAA\n12\n',1,'2023-09-30 14:15:16'),(16,'222','2222222\n',1,'2023-09-30 14:19:46'),(17,'1982892819282','2121222\n',1,'2023-09-30 14:21:54'),(19,'sass','sassss\n',1,'2023-09-30 14:31:47'),(20,'ssssss','12222222222222222222\n',1,'2023-09-30 14:34:39'),(21,'TestLuis','TestLuis\n',1,'2023-10-01 15:50:41'),(22,'Danieltest','Danieltest\n',1,'2023-10-01 17:01:37');
 /*!40000 ALTER TABLE `project` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -163,7 +211,7 @@ CREATE TABLE `project_files` (
   KEY `File_ID` (`File_ID`),
   CONSTRAINT `project_files_ibfk_1` FOREIGN KEY (`Id_Project`) REFERENCES `project` (`Id_Project`),
   CONSTRAINT `project_files_ibfk_2` FOREIGN KEY (`File_ID`) REFERENCES `files` (`File_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -172,6 +220,7 @@ CREATE TABLE `project_files` (
 
 LOCK TABLES `project_files` WRITE;
 /*!40000 ALTER TABLE `project_files` DISABLE KEYS */;
+INSERT INTO `project_files` VALUES (8,21,9);
 /*!40000 ALTER TABLE `project_files` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -183,14 +232,14 @@ DROP TABLE IF EXISTS `question`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `question` (
-  `Id_Question` int NOT NULL,
+  `Id_Question` int NOT NULL AUTO_INCREMENT,
   `TextQuestion` varchar(600) DEFAULT NULL,
   `TypeQuestion` varchar(45) DEFAULT NULL,
   `Id_Poll` int NOT NULL,
   `Id_Question_Type` int NOT NULL,
   PRIMARY KEY (`Id_Question`),
-  KEY `Fk_Poll_idx` (`Id_Poll`),
   KEY `Fk_Question_Type_idx` (`Id_Question_Type`),
+  KEY `Fk_Poll_idx` (`Id_Poll`),
   CONSTRAINT `Fk_Poll` FOREIGN KEY (`Id_Poll`) REFERENCES `poll` (`Id_Poll`),
   CONSTRAINT `Fk_Question_Type` FOREIGN KEY (`Id_Question_Type`) REFERENCES `question_type` (`Id_Question_Type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -213,12 +262,11 @@ DROP TABLE IF EXISTS `question_options`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `question_options` (
-  `Id_Question_Option` int NOT NULL,
+  `Id_Question_Option` int NOT NULL AUTO_INCREMENT,
   `Id_Question` int NOT NULL,
   `Option_Text` int NOT NULL,
-  `Option_Counter` int DEFAULT NULL,
   PRIMARY KEY (`Id_Question_Option`),
-  KEY `Fk_Answer_idx` (`Id_Question`),
+  KEY `Fk_Question_idx` (`Id_Question`),
   CONSTRAINT `Fk_Question` FOREIGN KEY (`Id_Question`) REFERENCES `question` (`Id_Question`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -282,7 +330,7 @@ CREATE TABLE `seg_usu` (
 
 LOCK TABLES `seg_usu` WRITE;
 /*!40000 ALTER TABLE `seg_usu` DISABLE KEYS */;
-INSERT INTO `seg_usu` VALUES ('cristian','Christian Fabian','12345','Administrador','2023/07/26',8888,'cristianfcg18gmail.com',1,1),('jaguilar','Johan Aguilar','12345','Administrador','2023/07/26',9999,'jaguilar@technosoftcr.com',1,1);
+INSERT INTO `seg_usu` VALUES ('adrian','Adrian','12345','Administrador','2023/07/26',6666,'adrian@gmail.com',1,1),('cristian','Christian Fabian','12345','Administrador','2023/07/26',8888,'cristianfcg18gmail.com',1,1),('jaguilar','Johan Aguilar','12345','Administrador','2023/07/26',9999,'jaguilar@technosoftcr.com',1,1),('luis','Luis Perez','12345','Administrador','2023/07/26',7777,'ldperez.0711@gmail.com',1,1);
 /*!40000 ALTER TABLE `seg_usu` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -302,7 +350,7 @@ CREATE TABLE `seg_usu_project` (
   KEY `Fk_Id_Project_idx` (`Id_Project`),
   CONSTRAINT `Fk_Id_Project` FOREIGN KEY (`Id_Project`) REFERENCES `project` (`Id_Project`),
   CONSTRAINT `Fk_User_Login` FOREIGN KEY (`User_Login`) REFERENCES `seg_usu` (`usu_Login`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -311,6 +359,7 @@ CREATE TABLE `seg_usu_project` (
 
 LOCK TABLES `seg_usu_project` WRITE;
 /*!40000 ALTER TABLE `seg_usu_project` DISABLE KEYS */;
+INSERT INTO `seg_usu_project` VALUES (16,'cristian',21),(18,'cristian',22);
 /*!40000 ALTER TABLE `seg_usu_project` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -322,21 +371,21 @@ DROP TABLE IF EXISTS `sprint`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `sprint` (
-  `Id_Sprint` int NOT NULL,
+  `Id_Sprint` int NOT NULL AUTO_INCREMENT,
   `Sprint_Name` varchar(45) DEFAULT NULL,
   `Start_Date` datetime DEFAULT NULL,
-  `id_Proyect` int NOT NULL,
-  `sprintcol` varchar(45) NOT NULL,
-  `Id_State` int NOT NULL,
+  `End_Date` datetime DEFAULT NULL,
+  `Id_Project` int NOT NULL,
+  `Id_Status` int NOT NULL,
   `User_Login` varchar(20) NOT NULL,
   PRIMARY KEY (`Id_Sprint`),
-  KEY `Fk_Project_idx` (`id_Proyect`),
-  KEY `Fk_State_Sprint_idx` (`Id_State`),
+  KEY `Fk_Project_idx` (`Id_Project`),
+  KEY `Fk_Status_Sprint_idx` (`Id_Status`),
   KEY `Fk_User_idx` (`User_Login`),
-  CONSTRAINT `Fk_Project` FOREIGN KEY (`id_Proyect`) REFERENCES `project` (`Id_Project`),
-  CONSTRAINT `Fk_State_Sprint` FOREIGN KEY (`Id_State`) REFERENCES `state` (`Id_State`),
+  CONSTRAINT `Fk_Project` FOREIGN KEY (`Id_Project`) REFERENCES `project` (`Id_Project`),
+  CONSTRAINT `Fk_Status_Sprint` FOREIGN KEY (`Id_Status`) REFERENCES `status` (`Id_Status`),
   CONSTRAINT `Fk_User` FOREIGN KEY (`User_Login`) REFERENCES `seg_usu` (`usu_Login`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -345,31 +394,32 @@ CREATE TABLE `sprint` (
 
 LOCK TABLES `sprint` WRITE;
 /*!40000 ALTER TABLE `sprint` DISABLE KEYS */;
+INSERT INTO `sprint` VALUES (1,'Test_Sprint','2022-10-01 00:00:00','2022-10-05 00:00:00',15,1,'cristian'),(2,'Test_Sprint2','2022-09-30 00:00:00','2022-10-05 00:00:00',16,1,'jaguilar');
 /*!40000 ALTER TABLE `sprint` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `state`
+-- Table structure for table `status`
 --
 
-DROP TABLE IF EXISTS `state`;
+DROP TABLE IF EXISTS `status`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `state` (
-  `Id_State` int NOT NULL,
-  `State_Name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`Id_State`)
+CREATE TABLE `status` (
+  `Id_Status` int NOT NULL,
+  `Status_Name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id_Status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `state`
+-- Dumping data for table `status`
 --
 
-LOCK TABLES `state` WRITE;
-/*!40000 ALTER TABLE `state` DISABLE KEYS */;
-INSERT INTO `state` VALUES (1,'Activo'),(2,'Inactivo');
-/*!40000 ALTER TABLE `state` ENABLE KEYS */;
+LOCK TABLES `status` WRITE;
+/*!40000 ALTER TABLE `status` DISABLE KEYS */;
+INSERT INTO `status` VALUES (1,'Activo'),(2,'Inactivo');
+/*!40000 ALTER TABLE `status` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -380,19 +430,19 @@ DROP TABLE IF EXISTS `sub_task`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `sub_task` (
-  `Id_Sub_Task` int NOT NULL,
+  `Id_Sub_Task` int NOT NULL AUTO_INCREMENT,
   `Titule` varchar(45) DEFAULT NULL,
   `Description` varchar(300) DEFAULT NULL,
   `Requirid_Time` int DEFAULT NULL,
   `Id_Task` int NOT NULL,
-  `Id_State` int NOT NULL,
+  `Id_Status` int NOT NULL,
   `Id_Priority` int NOT NULL,
   PRIMARY KEY (`Id_Sub_Task`),
-  KEY `Fk_Task_idx` (`Id_Task`),
-  KEY `Fk_State_idx` (`Id_State`),
+  KEY `Fk_Status_idx` (`Id_Status`),
   KEY `Fk_Priority_idx` (`Id_Priority`),
+  KEY `Fk_Task_idx` (`Id_Task`),
   CONSTRAINT `Fk_Priority` FOREIGN KEY (`Id_Priority`) REFERENCES `priority` (`Id_Priority`),
-  CONSTRAINT `Fk_State` FOREIGN KEY (`Id_State`) REFERENCES `state` (`Id_State`),
+  CONSTRAINT `Fk_Status` FOREIGN KEY (`Id_Status`) REFERENCES `status` (`Id_Status`),
   CONSTRAINT `Fk_Task` FOREIGN KEY (`Id_Task`) REFERENCES `task` (`Id_Task`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -414,16 +464,17 @@ DROP TABLE IF EXISTS `task`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `task` (
-  `Id_Task` int NOT NULL,
+  `Id_Task` int NOT NULL AUTO_INCREMENT,
   `Task_Name` varchar(45) DEFAULT NULL,
   `Description_Task` varchar(300) DEFAULT NULL,
   `Id_Sprint` int NOT NULL,
-  `Id_State` int DEFAULT NULL,
+  `Id_Status` int NOT NULL,
+  `Task_Status` varchar(45) DEFAULT 'PENDIENTE',
   PRIMARY KEY (`Id_Task`),
+  KEY `Fk_Status_Task_idx` (`Id_Status`),
   KEY `Fk_Sprint_idx` (`Id_Sprint`),
-  KEY `Fk_State_Task_idx` (`Id_State`),
   CONSTRAINT `Fk_Sprint` FOREIGN KEY (`Id_Sprint`) REFERENCES `sprint` (`Id_Sprint`),
-  CONSTRAINT `Fk_State_Task` FOREIGN KEY (`Id_State`) REFERENCES `state` (`Id_State`)
+  CONSTRAINT `Fk_Status_Task` FOREIGN KEY (`Id_Status`) REFERENCES `status` (`Id_Status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -435,14 +486,6 @@ LOCK TABLES `task` WRITE;
 /*!40000 ALTER TABLE `task` DISABLE KEYS */;
 /*!40000 ALTER TABLE `task` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'procormi'
---
-
---
--- Dumping routines for database 'procormi'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -453,4 +496,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-09-30 14:43:17
+-- Dump completed on 2023-10-02 20:37:54
